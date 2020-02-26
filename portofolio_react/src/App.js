d28b0070189e5e4d91cb8720ad477304231cb842
arip1997 <ariefr.santoso@gmail.com>
2020-02-27 01:24:04 +0700

    add home an add pict

--- portofolio_react/src/App.js
+++ portofolio_react/src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Contact from './Contact';
-import Project from './Project';
-import Resume from './Resume';
-import LandingPage from './LandingPage';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/Main';
+import {Link} from 'react-router-dom';
 
-const Main = () => (
-    <Switch>
-        <Route exact path ='/' component={LandingPage} />        
-        <Route exact path ='/contact' component={Contact} />
-        {/* <Route exact path ='/project' component={Project} /> */}
-        <Route exact path ='/resume' component={Resume} />
-    </Switch>
-)
 
-export default Main;
+
+function App() {
+  return (
+    <div className="demo-big-content">
+        <Layout>
+            <Header className='header-color' title={<Link style={{textDecoration:'none', color:'white'}} to="/">My Portofolio </Link>} scroll>            
+                <Navigation>                           
+                    <Link to="/contact">Contact</Link>            
+                    {/* <Link to="/project">Projects</Link> */}
+                    <Link to="/resume">Resume</Link>
+                </Navigation>
+            </Header>
+            <Drawer title={<Link to="/" style={{textDecoration:'none', color:'grey'}}>My Portofolio</Link>}>
+                <Navigation>                                        
+                    <Link to="/contact">Contact</Link>            
+                    {/* <Link to="/project">Projects</Link> */}
+                    <Link to="/resume">Resume</Link>
+                </Navigation>
+            </Drawer>
+            <Content>
+                
+                <Main />
+            </Content>
+        </Layout>
+    </div>
+  );
+}
+
+export default App;
