31cf6075a0b259199baf7cf42e7359dce592e1c2
arip1997 <ariefr.santoso@gmail.com>
2020-03-05 16:08:18 +0700

    add Post.jsx

---
+++ Minggu5/interaksi-api/src/components/BlogPost/Post.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+cons Post = (props) => {
+    return (
+        <div className="artikel">
+            <div classNamae="gambar-artikel">
+                
+            </div>
+        </div>
+    )
+}
